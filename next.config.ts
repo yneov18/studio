6115be92f40479060fd27b2f863351860a37fa31
LARBI BENAMAR <yneov18@gmail.com>
2025-08-05 14:43:45 +0000

    ÉTAPE 2 — Configuration pour mode offline 1. Installe le module PWA Dans

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type {NextConfig} from 'next';
+
 const withPWA = require('@ducanh2912/next-pwa').default({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+  runtimeCaching: [
+    {
+      urlPattern: /^https?.*/,
+      handler: 'NetworkFirst',
+      options: {
+        cacheName: 'offlineCache',
+        expiration: {
+          maxEntries: 200,
+        },
+      },
+    },
+  ],
 });
 
 const nextConfig: NextConfig = {
